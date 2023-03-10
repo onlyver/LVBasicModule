ee85ee6af7a5f5646f4367b0e38bd6530049daea
onlyver <1072539675@qq.com>
2023-03-10 10:39:38 +0800

    init

---
+++ LVBasicModule/Classes/BasicMain.h
@@ -0,0 +1,16 @@
+//
+//  BasicMain.h
+//  LVBasicModule
+//
+//  Created by LiLver on 2023/3/10.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface BasicMain : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ LVBasicModule/Classes/BasicMain.m
@@ -0,0 +1,12 @@
+//
+//  BasicMain.m
+//  LVBasicModule
+//
+//  Created by LiLver on 2023/3/10.
+//
+
+#import "BasicMain.h"
+
+@implementation BasicMain
+
+@end
